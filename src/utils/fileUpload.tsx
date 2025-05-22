0da9ae9990790f7eef656a3270e8a685806e6ec9
nguyentankdb17 <nguyentankdb17@gmail.com>
2025-05-22 16:13:01 +0700

    feat: add loading effect

--- src/utils/fileUpload.tsx
+++ src/utils/fileUpload.tsx
@@ -2,12 +2,16 @@ import { getAuth } from "firebase/auth";
 import React, { useEffect, useRef, useState } from "react";
 import DropdownButton from "./dropdownButton";
 import ImagePreview from "./imagePreview";
+import Loading from "./Loading";
 import SuccessMessage from "./successMessage";
+
 const FileUpload: React.FC = () => {
     const fileInputRef = useRef<HTMLInputElement | null>(null);
     const [file, setFile] = useState<File | null>(null);
     const [uploadedBytes, setUploadedBytes] = useState(0);
     const [messageVisible, setMessageVisible] = useState(false);
+    const [loadingVisible, setLoadingVisible] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
     const handleButtonClick = () => {
         fileInputRef.current?.click();
@@ -26,8 +30,11 @@ const FileUpload: React.FC = () => {
         const user = auth.currentUser;
         if (!user) {
             console.error("No user logged in");
+            setErrorMessage("User not logged in, please log in to continue using our service.");
             throw new Error("User not logged in");
         }
+
+        setLoadingVisible(true);
 
         const idToken = await user.getIdToken(); // Lấy token Firebase
         console.log("ID Token for upload:", idToken); // Debug token
@@ -43,14 +50,18 @@ const FileUpload: React.FC = () => {
                 },
                 body: formData,
             });
+            setLoadingVisible(false);
 
             if (!response.ok) {
-                const errorText = await response.text();
+                const errorText = await response.json();
+                setErrorMessage(errorText.message);
                 console.error("Server error:", response.status, errorText);
                 throw new Error("File upload failed");
             }
 
             setMessageVisible(true);
+            setFile(null);
+            setUploadedBytes(0);
             console.log("File uploaded successfully:", await response.json());
         } catch (error) {
             console.error("Error uploading file:", error);
@@ -80,46 +91,58 @@ const FileUpload: React.FC = () => {
         return () => clearInterval(interval);
     }, [file]);
 
+    if (loadingVisible) {
+        return (
+            <div className="flex h-full items-center justify-center bg-[#0F172A] opacity-100">
+                <Loading />
+            </div>
+        );
+    }
+
     return (
         <section className="bg-dark py-15">
             <div className="container">
                 <div className="relative mx-auto w-full max-w-[570px] rounded-[20px] bg-white/10 p-4 shadow-lg">
                     <div className="border-dark-6 relative z-10 flex min-h-[328px] items-center justify-center rounded-2xl border border-dashed bg-white/10 p-6 md:p-10">
                         <div className="w-full text-center">
-                            <input
-                                type="file"
-                                ref={fileInputRef}
-                                onChange={handleFileChange}
-                                className="sr-only"
-                                accept=".png,.jpg,.jpeg,.webp,.svg"
-                            />
-                            <div
-                                className="text-dark mx-auto mb-5 flex aspect-square w-[68px] cursor-pointer items-center justify-center rounded-full bg-white"
-                                onClick={handleButtonClick}
-                            >
-                                <svg
-                                    width="28"
-                                    height="28"
-                                    viewBox="0 0 28 28"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                >
-                                    <path
-                                        d="M24.5438 4.85623H14.4376L13.5188 3.10623C13.0376 2.23123 12.1626 1.66248 11.1563 1.66248H3.45635C1.96885 1.66248 0.787598 2.84373 0.787598 4.33123V23.6687C0.787598 25.1562 1.96885 26.3375 3.45635 26.3375H24.5876C26.0751 26.3375 27.2563 25.1562 27.2563 23.6687V7.52498C27.2563 6.03748 26.0313 4.85623 24.5438 4.85623ZM25.2876 23.6687C25.2876 24.0625 24.9813 24.3687 24.5876 24.3687H3.45635C3.0626 24.3687 2.75635 24.0625 2.75635 23.6687V4.33123C2.75635 3.93748 3.0626 3.63123 3.45635 3.63123H11.1563C11.4188 3.63123 11.6376 3.76248 11.7688 4.02498L12.9938 6.29998C13.1688 6.60623 13.5188 6.82498 13.8688 6.82498H24.5876C24.9813 6.82498 25.2876 7.13123 25.2876 7.52498V23.6687Z"
-                                        fill="currentColor"
+                            {!file && (
+                                <div>
+                                    <input
+                                        type="file"
+                                        ref={fileInputRef}
+                                        onChange={handleFileChange}
+                                        className="sr-only"
+                                        accept=".png,.jpg,.jpeg,.webp,.svg"
                                     />
-                                    <path
-                                        d="M14.7 10.675C14.3063 10.2812 13.6938 10.2812 13.3 10.675L9.4938 14.4375C9.10005 14.8312 9.10005 15.4437 9.4938 15.8375C9.88755 16.2312 10.5 16.2312 10.8938 15.8375L13.0375 13.7375V20.125C13.0375 20.65 13.475 21.1312 14.0438 21.1312C14.6125 21.1312 15.0063 20.6937 15.0063 20.125V13.6937L17.1938 15.8375C17.3688 16.0125 17.6313 16.1 17.8938 16.1C18.1563 16.1 18.4188 16.0125 18.5938 15.7937C18.9875 15.4 18.9875 14.7875 18.5938 14.3937L14.7 10.675Z"
-                                        fill="currentColor"
-                                    />
-                                </svg>
-                            </div>
-                            <div className="mx-auto w-full max-w-[290px] text-center">
-                                <h3 className="mb-3 text-xl font-bold text-white">Drop File Here</h3>
-                                <p className="mb-5 text-base text-gray-400">
-                                    Drag and drop your PNG, JPG, WebP, SVG images here or browse
-                                </p>
+                                    <div
+                                        className="text-dark mx-auto mb-5 flex aspect-square w-[68px] cursor-pointer items-center justify-center rounded-full bg-white"
+                                        onClick={handleButtonClick}
+                                    >
+                                        <svg
+                                            width="28"
+                                            height="28"
+                                            viewBox="0 0 28 28"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M24.5438 4.85623H14.4376L13.5188 3.10623C13.0376 2.23123 12.1626 1.66248 11.1563 1.66248H3.45635C1.96885 1.66248 0.787598 2.84373 0.787598 4.33123V23.6687C0.787598 25.1562 1.96885 26.3375 3.45635 26.3375H24.5876C26.0751 26.3375 27.2563 25.1562 27.2563 23.6687V7.52498C27.2563 6.03748 26.0313 4.85623 24.5438 4.85623ZM25.2876 23.6687C25.2876 24.0625 24.9813 24.3687 24.5876 24.3687H3.45635C3.0626 24.3687 2.75635 24.0625 2.75635 23.6687V4.33123C2.75635 3.93748 3.0626 3.63123 3.45635 3.63123H11.1563C11.4188 3.63123 11.6376 3.76248 11.7688 4.02498L12.9938 6.29998C13.1688 6.60623 13.5188 6.82498 13.8688 6.82498H24.5876C24.9813 6.82498 25.2876 7.13123 25.2876 7.52498V23.6687Z"
+                                                fill="currentColor"
+                                            />
+                                            <path
+                                                d="M14.7 10.675C14.3063 10.2812 13.6938 10.2812 13.3 10.675L9.4938 14.4375C9.10005 14.8312 9.10005 15.4437 9.4938 15.8375C9.88755 16.2312 10.5 16.2312 10.8938 15.8375L13.0375 13.7375V20.125C13.0375 20.65 13.475 21.1312 14.0438 21.1312C14.6125 21.1312 15.0063 20.6937 15.0063 20.125V13.6937L17.1938 15.8375C17.3688 16.0125 17.6313 16.1 17.8938 16.1C18.1563 16.1 18.4188 16.0125 18.5938 15.7937C18.9875 15.4 18.9875 14.7875 18.5938 14.3937L14.7 10.675Z"
+                                                fill="currentColor"
+                                            />
+                                        </svg>
+                                    </div>
 
+                                    <h3 className="mb-3 text-xl font-bold text-white">Drop File Here</h3>
+                                    <p className="mb-5 max-w-sm text-base text-gray-400">
+                                        Drag and drop your PNG, JPG, WebP, SVG image here or browse
+                                    </p>
+                                </div>
+                            )}
+                            <div className="mx-auto w-full max-w-[290px] text-center">
                                 {file && (
                                     <ImagePreview
                                         file={file}
@@ -139,6 +162,7 @@ const FileUpload: React.FC = () => {
                                 <div className="z-10 w-full">
                                     <DropdownButton onClick={handleUpload} />
                                 </div>
+                                {errorMessage != "" && <div className="mt-4 text-sm text-red-500">{errorMessage}</div>}
                             </div>
                         </div>
                     </div>
