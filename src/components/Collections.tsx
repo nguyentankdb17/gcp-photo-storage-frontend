55cf46ea0099e69782dffe62f1b1be9e3e8b831a
nguyentankdb17 <nguyentankdb17@gmail.com>
2025-05-26 10:44:55 +0700

    fix: update prettier style

--- src/components/Collections.tsx
+++ src/components/Collections.tsx
@@ -111,9 +111,7 @@ const Collections: React.FC = () => {
         }
 
         const filtered = images.filter((image) =>
-            image.labels.some((label) =>
-                label.description.toLowerCase().includes(searchTerm.toLowerCase())
-            )
+            image.labels.some((label) => label.description.toLowerCase().includes(searchTerm.toLowerCase())),
         );
 
         setFilteredImages(filtered);
@@ -124,7 +122,6 @@ const Collections: React.FC = () => {
             setNoResults(false);
         }
     };
-
 
     //add the userId to see what images belong to the user command it to show all the old function
     useEffect(() => {
@@ -203,17 +200,28 @@ const Collections: React.FC = () => {
                     <h1 className="py-5 text-4xl font-bold text-white">Your Image Collections</h1>
                 </div>
 
-                <div className="flex my-8 rounded-md border-2 border-blue-500 overflow-hidden max-w-md w-full mx-auto">
-                    <input type="text" id="searchbar" placeholder="Search description..." onChange={(e) => searchImages(e.target.value)}
-                    className="w-full outline-none bg-white text-black text-sm px-4 py-3" />
-                    <button type='button' 
+                <div className="mx-auto my-8 flex w-full max-w-md overflow-hidden rounded-md border-2 border-blue-500">
+                    <input
+                        type="text"
+                        id="searchbar"
+                        placeholder="Search description..."
+                        onChange={(e) => searchImages(e.target.value)}
+                        className="w-full bg-white px-4 py-3 text-sm text-black outline-none"
+                    />
+                    <button
+                        type="button"
                         className="flex cursor-pointer items-center justify-center bg-[#007bff] px-5"
-                        onClick={() => searchImages((document.getElementById("searchbar") as HTMLInputElement)?.value || "")}
+                        onClick={() =>
+                            searchImages((document.getElementById("searchbar") as HTMLInputElement)?.value || "")
+                        }
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 192.904 192.904"
+                            width="16px"
+                            className="fill-white"
                         >
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px" className="fill-white">
-                            <path
-                            d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
-                            </path>
+                            <path d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"></path>
                         </svg>
                     </button>
                 </div>
